04de0e9f88458796f0fc09a8ead191f2fa23ac94
ezrahel <adelakinisrael024@gmail.com>
2023-10-12 00:34:09 +0100

    adjusted

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	db.setupDB()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading the env file ")
+	}
+	port := os.Getenv("DB_PORT")
+	app := fiber.New()
+	app.Listen(":" + port)
 }
